fd6aaffc6f986df697e18f25ebb0cb5244deba86
Veronica <veromartina8@gmail.com>
2024-12-06 00:24:34 -0300

    se agregan imagenes y videos, se eliminan archivos ,se limpian archivos

--- TpTodoList/src/main.jsx
+++ TpTodoList/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { ChakraProvider, extendTheme} from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ChakraProvider  theme={extendTheme}>
     <App />
+    </ChakraProvider>
   </StrictMode>,
 )
