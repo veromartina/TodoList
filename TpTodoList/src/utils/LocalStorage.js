9d7cd6d740dfe538118fc697ecf19ff04ab3d2c6
Veronica <veromartina8@gmail.com>
2024-12-06 00:33:21 -0300

    se crean archivos de componentes,y local storage

---
+++ TpTodoList/src/utils/LocalStorage.js
@@ -0,0 +1,8 @@
+export const getTasks = () => {
+    const tasks = localStorage.getItem('tasks');
+    return tasks ? JSON.parse(tasks) : [];
+  };
+  
+  export const saveTasks = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  };
